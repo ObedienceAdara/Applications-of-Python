fd879b2d41f2bb759d9943a45ceba7d96870daa3
Obedience Adara <166041189+ObedienceAdara@users.noreply.github.com>
2024-07-28 14:10:54 +0100

    Update main.py

--- AI and Machine Learning/Predicting Iris Flower Species/main.py
+++ AI and Machine Learning/Predicting Iris Flower Species/main.py
@@ -1,1 +1,10 @@
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=33, test_size=0.2)
+
